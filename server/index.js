f79aa55ccf197a01eb60441533fc9f23f861d7b9
Jake <jake.c.gardner@gmail.com>
2019-08-28 15:24:57 +0900

    add posts

--- server/index.js
+++ server/index.js
@@ -3,20 +3,35 @@ const { GraphQLServer } = require('graphql-yoga');
 let users = [];
 let idCount = 0;
 
+let posts = [];
+let postCount = 0;
+
 const resolvers = {
   Query: {
     users: () => users,
     user: (parent, args) => users.find(us => us.id === args.id),
+    posts: () => posts,
+    userPosts: (parent, args) => posts.find(pst => pst.userId === args.userId),
   },
   Mutation: {
-    createUser: (parent, args) => {
-       const user = {
+    createUser: (parent, { name, age }) => {
+      const user = {
         id: `user-${idCount++}`,
-        name: args.name,
-        age: args.age,
+        name,
+        age,
       };
       users.push(user);
       return user;
+    },
+    createPost: (parent, { date, userId, content }) => {
+      const post = {
+        id: `post-${idCount++}`,
+        date,
+        userId,
+        content,
+      };
+      posts.push(post);
+      return post;
     }
   },
 };
